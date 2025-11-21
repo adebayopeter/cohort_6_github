945c5d5afb89341a44543d9a1bc1912772d26d34
Adebayo Olaonipekun <pekunmi@live.com>
2025-11-21 18:09:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+# GUI
+# Terminal / Console
+
+import os
+
+username = os.getenv('EMAIL_USERNAME')
+password = os.getenv('EMAIL_PASSWORD')
+
+
+def open_gmail(user, pwd):
+    return f'{user, pwd}'
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
